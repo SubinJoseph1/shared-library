f10598c56ec30146eff615ed27ff77579fc43248
Subin <subin.j@idp.com>
2024-04-30 15:26:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MySharedComponentsComponent } from 'my-shared-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MySharedComponentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
